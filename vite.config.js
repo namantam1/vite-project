4d346e6e2dee79bb6d361b13af774844963db684
Naman Tamrakar <namantam1@gmail.com>
2022-02-23 16:18:16 +0530

    Add glass morphic ui here

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,17 @@ const { resolve } = require("path");
  * @type {import('vite').UserConfig}
  */
 const config = {
-    root: "src",
-    build: {
-        outDir: "../dist",
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, "src/index.html"),
-                nested: resolve(__dirname, "src/scroll-effect/index.html"),
-            },
-        },
+  root: "src",
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        scrollEffect: resolve(__dirname, "src/scroll-effect/index.html"),
+        glass: resolve(__dirname, "src/glass-morphic/index.html"),
+      },
     },
+  }
 };
 
 export default config;
